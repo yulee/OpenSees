56a4ac30d276752f9e492d17ba635b06e9095b16
fsiale <fsiale@gmail.com>
2024-12-20 21:02:23 -0800

    Add differentiate() to GroundMotion 11

--- EXAMPLES/ExamplePython/example_ground_motion.py
+++ EXAMPLES/ExamplePython/example_ground_motion.py
@@ -31,7 +31,7 @@ ops.timeSeries('Path', 3, '-time', *time_values.tolist(), '-values', *disp_value
 ts_type = ["-accel", "-vel", "-disp"]
 plt.figure()
 
-for i in range(1, 4):     
+for i in range(1, 3):     
     print(f"Running case {i}")
     
     # Apply uniform excitation pattern
