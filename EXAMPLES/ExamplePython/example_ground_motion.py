59d1c8470b54c97572f7be3f7a6d238864b73df8
fsiale <fsiale@gmail.com>
2025-01-31 20:21:46 -0800

    damping parameter

--- EXAMPLES/ExamplePython/example_ground_motion.py
+++ EXAMPLES/ExamplePython/example_ground_motion.py
@@ -2,8 +2,8 @@ import sys
 sys.path.insert(0, '../../SRC/interpreter/')
 # sys.path.insert(0, '../build/lib/')
 import opensees as ops
-import math
 
+#######
 import matplotlib.pyplot as plt
 # import openseespy.opensees as ops
 import numpy as np
@@ -16,13 +16,13 @@ ops.node(2, 0.0); ops.mass(2, 1.0)
 ops.uniaxialMaterial('Elastic', 1, 100.0)
 ops.damping('Uniform', 1, 0.05, 1.0, 100.0)
 ops.element('zeroLength', 1, 1, 2, '-mat', 1, '-dir', 1, '-damp', 1)
-ops.timeSeries('Sine', 1, 0.0, 100.0, 0.2*math.pi, '-factor', -0.01)
+ops.timeSeries('Sine', 1, 0.0, 100.0, 0.628, '-factor', -0.01)
 ops.pattern('UniformExcitation', 1, 1, '-accel', 1)
 ops.analysis('Transient')
 ops.recorder('Node', '-file', 'disp.out', '-time', '-node', 2, '-dof', 1, 'disp')
 
-ops.analyze(500, 0.1)
-ops.analyze(500, 0.1)
+ops.analyze(500, 0.01)
+ops.analyze(500, 0.01)
 
 ops.wipe()
 data = np.loadtxt('disp.out')
@@ -30,11 +30,11 @@ time = data[:, 0]
 disp = data[:, 1]
 
 plt.figure()
-plt.plot(time, disp, label="disp", lw=2)
+plt.plot(time, disp)
 plt.xlabel("Time")
 plt.ylabel("Displacement")
-# plt.title("Comparison of Displacement for All Cases")
-# plt.legend()
 plt.grid()
 plt.show()
+
+#######
 plt.savefig('./build/lib/ground_motion.png')
