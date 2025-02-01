673c56db6e76431a6fbf0eeef257a0b41c9ef48c
fsiale <fsiale@gmail.com>
2025-01-31 20:11:03 -0800

    damping parameter

--- EXAMPLES/ExamplePython/example_ground_motion.py
+++ EXAMPLES/ExamplePython/example_ground_motion.py
@@ -13,7 +13,7 @@ ops.wipe()
 ops.model('basic', '-ndm', 1, '-ndf', 1)
 ops.node(1, 0.0);  ops.fix(1, 1)
 ops.node(2, 0.0); ops.mass(2, 1.0)
-ops.uniaxialMaterial('Elastic', 1, 1000.0)
+ops.uniaxialMaterial('Elastic', 1, 100.0)
 ops.damping('Uniform', 1, 0.05, 1.0, 100.0)
 ops.element('zeroLength', 1, 1, 2, '-mat', 1, '-dir', 1, '-damp', 1)
 ops.timeSeries('Sine', 1, 0.0, 100.0, 0.2*math.pi, '-factor', -0.01)
@@ -21,12 +21,8 @@ ops.pattern('UniformExcitation', 1, 1, '-accel', 1)
 ops.analysis('Transient')
 ops.recorder('Node', '-file', 'disp.out', '-time', '-node', 2, '-dof', 1, 'disp')
 
-T = 10
-dt = 0.01
-Nsteps = int(T/dt)
-
-for i in range(Nsteps):
-  ops.analyze(1,dt)
+ops.analyze(500, 0.1)
+ops.analyze(500, 0.1)
 
 ops.wipe()
 data = np.loadtxt('disp.out')
