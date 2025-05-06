bd08309a6d376fa0064ead001f915bdc7658cb46
fsiale <fsiale@gmail.com>
2025-05-05 19:53:59 -0700

    constraint01

--- EXAMPLES/ExamplePython/example_ground_motion.py
+++ EXAMPLES/ExamplePython/example_ground_motion.py
@@ -1,32 +1,27 @@
-# import sys
-# sys.path.insert(0, '../../SRC/interpreter/')
-# sys.path.insert(0, '../build/lib/')
-# import opensees as ops
-
-#######
 import matplotlib.pyplot as plt
 import openseespy.opensees as ops
 import numpy as np
 import vfo.vfo as vfo
 
 ops.wipe()
-ops.model('basic', '-ndm', 1, '-ndf', 1)
-ops.node(1, 0.0); ops.fix(1, 1)
-ops.node(2, 0.0);
-ops.node(3, 0.0);
-ops.node(4, 0.0);
+ops.model('basic', '-ndm', 2, '-ndf', 2)
+ops.node( 1, 0.0, 0.0); ops.fix( 1, 0, 1)
+ops.node(11, 0.0, 0.0); ops.fix(11, 1, 1)
+ops.node(12, 1.0, 0.0); ops.fix(12, 0, 1)
+ops.node(21, 0.0, 1.0); ops.fix(21, 1, 1)
+ops.node(22, 1.0, 1.0); ops.fix(22, 0, 1)
 ops.uniaxialMaterial('Elastic', 1, 100.0)
-ops.element('zeroLength', 1, 1, 2, '-mat', 1, '-dir', 1)
-ops.element('zeroLength', 2, 1, 3, '-mat', 1, '-dir', 1)
-# vfo.plot_model(filename='model.png')
-ops.equationConstraint(4,1,-3.0,2,1,1.0,3,1,2.0)
+ops.element('zeroLength', 1, 11, 12, '-mat', 1, '-dir', 1)
+ops.element('zeroLength', 2, 21, 22, '-mat', 1, '-dir', 1)
+vfo.plot_model(filename='model.png')
+ops.equationConstraint(1, 1, -3.0, 12, 1, 1.0, 22, 1, 2.0)
 ops.timeSeries('Linear',1)
 ops.pattern('Plain',1,1)
 ops.load(4,1.0)
 ops.integrator('LoadControl', 1.0)
 ops.constraints('Penalty', 1.0e10, 1.0e10)
 ops.analysis('Static')
-ops.recorder('Node', '-file', 'disp.out', '-time', '-node', 2, 3, '-dof', 1, 'disp')
+ops.recorder('Node', '-file', 'disp.out', '-time', '-node', 12, 22, '-dof', 1, 'disp')
 ops.analyze(10)
 ops.wipe()
 data = np.loadtxt('disp.out')
