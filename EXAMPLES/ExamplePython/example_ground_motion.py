892fbb211644d8c5035991d138075305fb6a7846
fsiale <fsiale@gmail.com>
2025-05-06 20:02:07 -0700

    constraint01

--- EXAMPLES/ExamplePython/example_ground_motion.py
+++ EXAMPLES/ExamplePython/example_ground_motion.py
@@ -1,8 +1,4 @@
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import openseespy.opensees as ops
-import numpy as np
 import utils.vfo.vfo as vfo
 
 ops.wipe()
@@ -25,7 +21,6 @@ for i in range(1, nrow):
         ops.element('quad', i * 10 + j, i * 10 + j, i * 10 + j1, i1 * 10 + j1, i1 * 10 + j, 1.0, 'PlaneStress', 1 if i == 1 and j == 1 else 2)
 
 vfo.createODB(model="model01", loadcase="static")
-vfo.plot_model(model = "model01", line_width = 5, filename = 'model')
 
 for i in range(1, nrow):
     ops.equationConstraint(i * 10 + ncol, 1, 1.0, i * 10 + 1, 1, -1.0, 1, 1, -1.0)
@@ -41,23 +36,12 @@ ops.equationConstraint(nrow * 10 + ncol, 2, 1.0, 10 + ncol, 2, -1.0, 1, 2, -1.0)
 
 ops.timeSeries('Linear',1)
 ops.pattern('Plain',1,1)
-ops.load(1, 1.0, 1.0)
-# ops.integrator('LoadControl', 1.0)
-ops.constraints('Penalty', 1.0e10, 1.0e10)
+ops.load(1, 10.0, 10.0)
+ops.constraints('Penalty', 1.0e6, 1.0e6)
 ops.analysis('Static')
 ops.recorder('Node', '-file', 'disp.out', '-time', '-node', 21, 31, '-dof', 1, 'disp')
-ops.analyze(10)
+ops.analyze(1)
 ops.wipe()
-print('Analysis completed')
-data = np.loadtxt('disp.out')
-plt.figure()
-plt.plot(data[:, 0], data[:, 1])
-plt.plot(data[:, 0], data[:, 2])
-plt.xlabel('Time')
-plt.ylabel('Displacement')
-plt.grid()
-# plt.show()
 
-#######
-plt.savefig('ground_motion.png')
-print('Plot completed')
+vfo.plot_deformedshape(model="model01", loadcase="static", line_width = 5, filename = 'model')
+
